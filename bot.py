782a418284401734589c448ec489830a326096b9
sneha <sneha.desai@mail.mcgill.ca>
2017-08-12 15:11:25 -0400

    updated generate_response function

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import json
 from watson_developer_cloud import ConversationV1 as Cv
+from pprint import pprint
 
 conversation = Cv(
   username="d430f35d-4d65-4fa0-aecf-e6aa784d0e4b",
@@ -11,11 +12,13 @@ def generate_reponse(input):
     input = input
     workspace_id = '61d5aa77-f337-4f32-b4b0-240a474fef13'
     context = {}
+    # message_input = {}
+    # message_input['text'] = input
     response = conversation.message(
       workspace_id=workspace_id,
-      message_input={'text': 'hi'},context=context)
+      message_input={'text': input},
+      context=context)
 
-    x = json.dumps(response, indent=2)
-    print x
+    return response["output"]["text"][0]
 
-generate_reponse(input)
+print generate_reponse('hi')
